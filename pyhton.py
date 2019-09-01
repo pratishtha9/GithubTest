250b1f36a63b86db7482f359c51270aeb00aa698
Pratishtha Singh <pratishtha918@gmail.com>
2019-09-01 12:23:46 +0530

    chnaged, added recursion

--- pyhton.py
+++ pyhton.py
@@ -1,2 +1,9 @@
-import random
-print(random.randrange(1,21))
+def recfunc(k):
+    if (k>0):
+        result=k+recfunc(k-1)
+        print(result)
+    else:
+        result=0
+    return result
+recfunc(7)
+
